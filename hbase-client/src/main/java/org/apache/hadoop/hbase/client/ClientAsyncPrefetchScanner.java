385fdc8cb1814e881ae830d8a2f49fbd0f37882a
eshcar <eshcar@yahoo-inc.com>
2015-02-28 22:08:46 +0200

    license header

--- hbase-client/src/main/java/org/apache/hadoop/hbase/client/ClientAsyncPrefetchScanner.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/client/ClientAsyncPrefetchScanner.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.hbase.client;
 
 import org.apache.hadoop.conf.Configuration;
@@ -20,9 +37,9 @@ public class ClientAsyncPrefetchScanner extends ClientScanner {
     // prefetch runnable object to be executed asynchronously
     private PrefetchRunnable prefetchRunnable;
     // Boolean flag to ensure only a single prefetch is running (per scan)
-    // eshcar: we use atomic boolean to allow multiple concurrent scanners to
-    // consume records from the same cache, but still have a single prefetcher.
-    // For a single scanner thread can replace this with a native boolean.
+    // eshcar: we use atomic boolean to allow multiple concurrent threads to
+    // consume records from the same cache, but still have a single prefetcher thread.
+    // For a single consumer thread this can be replace with a native boolean.
     private AtomicBoolean isPrefetchRunning = new AtomicBoolean(false);
     // Thread pool shared by all scanners
     private static final ExecutorService pool = Executors.newCachedThreadPool();
