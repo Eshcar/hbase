36bc1e24f45ab83d0889a8b9408bba45ad96f615
Anastasia Braginsky <anastas@ranbrought-lm.haifa.corp.yahoo.com>
2015-04-27 16:00:05 +0300

    Initial separate MemStoreScanner

---
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/MemStoreScanner.java
@@ -0,0 +1,79 @@
+package org.apache.hadoop.hbase.regionserver;
+
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.classification.InterfaceAudience;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * This is the scanner for any *MemStore implementation derived from MemStore,
+ * currently works for DefaultMemStore and CompactMemStore.
+ * The MemStoreScanner uses the set of CellSetMgrScanners
+ *
+ * Threads safety: It is assumed that CellSetMgr's underneath key-value set implementations is
+ * lock-free, therefore no special synchronization is required for concurrent scanners.
+ */
+@InterfaceAudience.Private
+class MemStoreScanner extends NonLazyKeyValueScanner {
+    private KeyValueHeap forwardHeap;
+    private ReversedKeyValueHeap backwardHeap;
+    private long readPoint;
+
+    MemStoreScanner(List<KeyValueScanner> scanners,
+                    KeyValue.KVComparator comparator,
+                    long readPoint) throws IOException {
+        this.readPoint = readPoint;
+        this.forwardHeap = new KeyValueHeap(scanners, comparator);
+        this.backwardHeap = new ReversedKeyValueHeap(scanners, comparator);
+    }
+
+    @Override
+    public Cell peek() {
+        return null;
+    }
+
+    @Override
+    public Cell next() throws IOException {
+        return null;
+    }
+
+    @Override
+    public boolean seek(Cell key) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean reseek(Cell key) throws IOException {
+        return false;
+    }
+
+    @Override
+    public long getSequenceID() {
+        return 0;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public boolean backwardSeek(Cell key) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean seekToPreviousRow(Cell key) throws IOException {
+        return false;
+    }
+
+    @Override
+    public boolean seekToLastRow() throws IOException {
+        return false;
+    }
+
+
+}
