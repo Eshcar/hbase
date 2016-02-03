985a9767f691544dc4dd4b44c2b9da6d8a2bdd5b
eshcar <eshcar@yahoo-inc.com>
2016-02-03 23:20:35 +0200

    remove imports

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/MutableCellSetSegmentScanner.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/MutableCellSetSegmentScanner.java
@@ -25,7 +25,6 @@ import java.util.SortedSet;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.classification.InterfaceAudience;
-import org.apache.hadoop.hbase.client.Scan;
 
 /**
  * A scanner of a single cells segment {@link MutableCellSetSegment}.
