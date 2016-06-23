c0dae4e9daef047b87fd9a18a6c4224938fcc511
anastas <anastas@yahoo-inc.com>
2016-06-23 14:21:06 +0300

    Debugging region doClose()

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/CompactingMemStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/CompactingMemStore.java
@@ -35,6 +35,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.ClassSize;
 import org.apache.hadoop.hbase.util.EnvironmentEdgeManager;
 import org.apache.hadoop.hbase.wal.WAL;
+import org.apache.hadoop.util.StringUtils;
 
 /**
  * A memstore implementation which supports in-memory compaction.
@@ -153,8 +154,20 @@ public class CompactingMemStore extends AbstractMemStore {
       LOG.warn("Snapshot called again without clearing previous. " +
           "Doing nothing. Another ongoing flush or did we fail last attempt?");
     } else {
+      int numOfPipeSegments = 0; int numOfPipeCells = 0; int internalSizes = 0;
+      for ( Segment s : pipeline.getSegments()) {
+        numOfPipeSegments++;
+        numOfPipeCells += s.getCellsCount();
+        internalSizes += s.getInternalSize();
+      }
       LOG.info("FLUSHING TO DISK: region "+ getRegionServices().getRegionInfo()
-          .getRegionNameAsString() + "store: "+ getFamilyName());
+          .getRegionNameAsString() + "store: "+ getFamilyName()
+          + "\n<<<<<< The compaction pipeline: " + pipeline.toString() + ". Pipeline's size: "
+          + pipeline.size() + ". Number of pipeline's segments: " + numOfPipeSegments
+          + " and cells: " + numOfPipeCells + " and internal sizes" + internalSizes
+          + "\n<<<<<< Is there in-memory-flush in progress? " + inMemoryFlushInProgress
+          + ". Active segment internal size: " + active.getInternalSize() + " and just size: "
+          + active.getSize() + ". Is active segment empty? " + active.isEmpty() );
       stopCompaction();
       pushActiveToPipeline(active);
       snapshotId = EnvironmentEdgeManager.currentTime();
@@ -262,7 +275,8 @@ public class CompactingMemStore extends AbstractMemStore {
       * in exclusive mode while this method (checkActiveSize) is invoked holding updatesLock
       * in the shared mode. */
       InMemoryFlushRunnable runnable = new InMemoryFlushRunnable();
-      LOG.info("Dispatching the MemStore in-memory flush for store " + store.getColumnFamilyName());
+      LOG.info("Dispatching the MemStore in-memory flush for store " + store.getColumnFamilyName()
+          + "; memstore=" + StringUtils.byteDesc(regionServices.getGlobalMemstoreTotalSize()));
       getPool().execute(runnable);
     }
   }
