7bd807c254d7bd27bb6a04c5830a7d0f7f238da9
anastas <anastas@yahoo-inc.com>
2017-02-23 22:55:08 +0200

    deb debu debug

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/CompactingMemStore.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/CompactingMemStore.java
@@ -83,7 +83,7 @@ public class CompactingMemStore extends AbstractMemStore {
       + 6 * ClassSize.REFERENCE     // Store, RegionServicesForStores, CompactionPipeline,
                                     // MemStoreCompactor, inMemoryFlushInProgress, allowCompaction
       + Bytes.SIZEOF_LONG           // inmemoryFlushSize
-      + 2 * Bytes.SIZEOF_BOOLEAN    // compositeSnapshot and inWalReplay
+      + 1 * Bytes.SIZEOF_BOOLEAN    // compositeSnapshot and inWalReplay
       + 2 * ClassSize.ATOMIC_BOOLEAN// inMemoryFlushInProgress and allowCompaction
       + CompactionPipeline.DEEP_OVERHEAD + MemStoreCompactor.DEEP_OVERHEAD);
 
