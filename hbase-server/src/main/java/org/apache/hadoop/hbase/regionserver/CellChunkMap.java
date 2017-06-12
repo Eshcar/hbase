a304b7d1eea28a5638a11e4cb16816a9fd813464
eshcar <eshcar@yahoo-inc.com>
2017-06-12 12:46:37 +0300

    remove diff with upstream

--- hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/CellChunkMap.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/regionserver/CellChunkMap.java
@@ -21,6 +21,7 @@ package org.apache.hadoop.hbase.regionserver;
 
 import java.nio.ByteBuffer;
 
+import org.apache.hadoop.hbase.shaded.com.google.common.annotations.VisibleForTesting;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.classification.InterfaceAudience;
 import org.apache.hadoop.hbase.util.Bytes;
